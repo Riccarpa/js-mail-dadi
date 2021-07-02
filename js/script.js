437d2ef06c6980aa66d724119e6778fd332e6297
Riccarpa <riccardopagni99@gmail.com>
2021-07-02 16:24:08 +0200

    loop done

--- js/script.js
+++ js/script.js
@@ -14,10 +14,26 @@ var email = prompt("inserisci la tua e-mail");
 var emailList = ["a@mail", "b@mail", "c@mail", "d@mail", "e@mail"];
 
 
+
 //matching
 
-if (email == emailList[0] || email == emailList[1] || email == emailList[2] || email == emailList[3] || email == emailList[4]) {
-    alert("welcome");
-} else {
-    alert("access denied");
+for (i = 0; i < emailList.length; i++) {
+    if (email == emailList[i]) {
+        var welcome = document.getElementById("welcome");
+        welcome.innerHTML = "Benvenuto: " + email;
+    }
 }
+
+
+
+
+
+
+/*Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto e stampare il risultato.
+*/
+
+//cpu number
+// var cpuNumber = Math.floor(Math.random() * 5) + 1;
+// console.log('numero random cpu: ', cpuNumber);
